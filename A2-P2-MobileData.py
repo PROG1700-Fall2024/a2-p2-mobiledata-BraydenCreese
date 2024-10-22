9cb8a8708924b857b6af3c56405c4adf22b96136
BraydenCreese <kidson2017@gmail.com>
2024-10-22 07:22:25 -0700

    Done.

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,18 +5,34 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #: W0491583 
+#Student Name: Brayden Creese
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    # getting user input and checking for invalid inputs
+    while True:
+        try:
+            dataAmount = float(input("Enter data usage (mb): "))
+            break
+        except ValueError:
+            print("Invalid Input! Please enter a valid number for data usage.")
 
 
+    # cacls for data rates / user input
+    if dataAmount <= 200:
+        totalCost = 20.00 # <--- flat rate for 200mb
+    elif dataAmount <= 500:
+        totalCost = dataAmount * 0.105 # <--- over 200-500 rate per mb
+    elif dataAmount <= 1000:
+        totalCost = dataAmount * 0.110  # <--- over 500-1Gb(1000) per mb
+    else:
+        totalCost = 118.00 # <--- flat rate over 1Gb
 
 
-
-
+    # print out results to user
+    print(f"\nTotal chrage is ${totalCost:.2f}")
 
     # YOUR CODE ENDS HERE
 
